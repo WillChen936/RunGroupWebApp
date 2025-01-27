bddb212166ea7dea6e5c03725a911c8a62f03d31
Will Chen <will.chen936@gmail.com>
2025-01-28 00:44:17 +0800

    feat: Add basic models

---
+++ RunGroupWebApp/Data/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroupWebApp.Data
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMaraton
+    }
+}
---
+++ RunGroupWebApp/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Street { get; set; }
+        public int City { get; set; }
+        public int State { get; set; }
+    }
+}
