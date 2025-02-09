6e74110081f49636e0371b810cd1b9ff6a5f0d52
Will Chen <will.chen936@gmail.com>
2025-02-09 22:42:01 +0800

    feat: Add delete page

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -113,5 +113,20 @@ namespace RunGroupWebApp.Controllers
                 return View(clubViewModel);
             }
         }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var clubDetails = await _clubRepository.GetByIdAsync(id);
+            if (clubDetails == null) return View("Error");
+            return View(clubDetails);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteClub(int id)
+        {
+            var clubDetails = await _clubRepository.GetByIdAsync(id);
+            if (clubDetails == null) return View("Error");
+
+            _clubRepository.Delete(clubDetails);
+            return RedirectToAction("Index");
+        }
     }
 }
