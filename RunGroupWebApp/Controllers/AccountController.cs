17268eda75d77db04d47ebec7080c4aff32c2d61
Will Chen <will.chen936@gmail.com>
2025-02-22 20:01:00 +0800

    feat: Add login page

---
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    return RedirectToAction("Index", "Race");
+                }
+                TempData["Error"] = "Wrong credentials. Please try again";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "No User found. Please try again";
+            return View(loginViewModel);
+        }
+    }
+}
