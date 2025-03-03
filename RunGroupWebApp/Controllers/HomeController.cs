d9f81da7cb27f74a6803b3c4ffefc8ea3741e650
Will Chen <will.chen936@gmail.com>
2025-03-04 01:11:40 +0800

    feat: Add IP Geo Location

--- RunGroupWebApp/Controllers/HomeController.cs
+++ RunGroupWebApp/Controllers/HomeController.cs
@@ -1,21 +1,53 @@
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroupWebApp.Helpers;
 using RunGroupWebApp.Models;
+using RunGroupWebApp.Repositories.Interfaces;
+using RunGroupWebApp.ViewModels;
 
 namespace RunGroupWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "http://ipinfo.io/118.150.85.81?token=aca9ba1c0fe9a7";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI = new RegionInfo(ipInfo.Country);
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = myRI.EnglishName;
+                if (homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetByCityAsync(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
